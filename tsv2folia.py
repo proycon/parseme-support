671f32b4d70f964e0d0d7d15a36da0ae28fca5b6
Maarten van Gompel <proycon@anaproy.nl>
2016-03-31 19:28:22 +0200

    fix

--- tsv2folia.py
+++ tsv2folia.py
@@ -18,19 +18,18 @@ for filename in sys.argv[1:]:
     doc = folia.Document(id=os.path.basename(filename.replace('.tsv','')))
     doc.declare(folia.Entity, "https://github.com/proycon/parseme-support/raw/master/parseme-mwe.foliaset.xml")
     text = doc.append(folia.Text)
-    sentence = text.append(folia.Sentence)
+    sentence = folia.Sentence(doc,generate_id_in=text)
     with open(filename,'r',encoding='utf-8') as f:
         for line in f:
             if line.strip(): #not empty
                 fields = line.strip().split('\t')
-                space = not (len(fields) >= 2 and fields[2] == 'nsp')
+                space = not (len(fields) > 2 and fields[2] == 'nsp')
                 sentence.append(folia.Word, text=fields[1],space=space)
             elif len(sentence) > 0: #empty and we have a sentence to add
                 text.append(sentence)
-                sentence = text.append(folia.Sentence)
-    if len(sentence) > 0: #don't forget the very last one
+                sentence = folia.Sentence(doc)
+    if sentence.count(folia.Word) > 0: #don't forget the very last one
         text.append(sentence)
-        sentence = text.append(folia.Sentence)
     doc.save(targetfilename)
 
 
