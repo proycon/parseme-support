dd42cc93e6dcd641fac25894dd999239bf9b913f
Federico Sangati <federico.sangati@gmail.com>
2016-10-21 12:08:30 +0200

    my first attempt to support pos

--- tsv2folia/tsv2folia.py
+++ tsv2folia/tsv2folia.py
@@ -8,13 +8,15 @@ except ImportError:
     print("ERROR: PyNLPl not found, please install pynlpl (pip install pynlpl)",file=sys.stderr)
     sys.exit(2)
 
+POS_SET_URL = "https://github.com/proycon/parseme-support/raw/master/parseme-pos.foliaset.xml"
 
 def convert(filename, targetfilename, rtl, set_file):
     doc = folia.Document(id=os.path.basename(filename.replace('.tsv','')))
     if rtl:
         doc.metadata['direction'] = 'rtl'
     doc.metadata['status'] = 'untouched'
-    doc.declare(folia.Entity, set_file)
+    doc.declare(folia.Entity, set_file) #ENTITY-SET definition    
+    doc.declare(folia.AnnotationType.POS, set=POS_SET_URL) #POS-SET definition 
     text = doc.append(folia.Text)
     sentence = folia.Sentence(doc,generate_id_in=text)
     with open(filename,'r',encoding='utf-8') as f:
@@ -23,6 +25,11 @@ def convert(filename, targetfilename, rtl, set_file):
                 fields = line.strip().split('\t')
                 space = not (len(fields) > 2 and fields[2] == 'nsp')
                 sentence.append(folia.Word, text=fields[1],space=space)
+                if len(fields) > 3 : # ONLY APPEND POS IF PRESENT
+                    posAnnot = folia.PosAnnotation(doc, cls=fields[3], 
+                                  annotator="auto",
+                                  annotatortype=folia.AnnotatorType.AUTO)
+                    sentence[-1].append(posAnnot)
             elif len(sentence) > 0: #empty and we have a sentence to add
                 text.append(sentence)
                 sentence = folia.Sentence(doc, generate_id_in=text)
@@ -47,7 +54,7 @@ def main():
     if len(sys.argv) == 1:
         print("Usage: tsv2folia.py [tsvfile] [[tsvfile]] ..etc..",file=sys.stderr)
         print("Add parameter --rtl for right-to-left languages (arabic, hebrew, farsi, etc)!")
-        print("Add parameter --set=parseme[-xx] for specifying a set file (e.g., --set=parseme-en)!")
+        print("Add parameter --set=xx for specifying a set file (e.g., --set=english)!")
         sys.exit(2)
 
     rtl = False
@@ -55,8 +62,9 @@ def main():
     set_path = 'https://github.com/proycon/parseme-support/raw/master/'
 
     set_options = {
-        "parseme": set_path + "parseme-mwe.foliaset.xml",
+        #"parseme": set_path + "parseme-mwe.foliaset.xml",
         "english": set_path + "parseme-mwe-en.foliaset.xml",
+        "farsi": set_path + "parseme-mwe-farsi.foliaset.xml",
         "germanic": set_path + "parseme-mwe-germanic.foliaset.xml",        
         "hebrew": set_path + "parseme-mwe-hebrew.foliaset.xml",
         "italian": set_path + "parseme-mwe-it.foliaset.xml",
@@ -67,8 +75,9 @@ def main():
         "yiddish": set_path + "parseme-mwe-yiddish.foliaset.xml",
     }
 
-    for filename in sys.argv[1:]:
-        set_file = set_options["parseme"]
+    set_file = None
+
+    for filename in sys.argv[1:]:        
         if filename == '--rtl':
             rtl = True
             continue
@@ -78,7 +87,12 @@ def main():
                 set_file = set_options[option]
                 continue
             else:
-                print("Wrong set file '{}'".format(option))
+                print("Wrong category-set configuration '{}'".format(option), file=sys.stderr))
+                sys.exit(2)
+        else:
+            pass
+        if set_file == None:
+            print("You did not specify a category-set configuration")
             sys.exit(2)
         targetfilename = filename.replace('.tsv','') + '.folia.xml'
         convert(filename, targetfilename, rtl, set_file)
